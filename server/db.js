f2ccd04e9b68fa54943d5f2390e26e0b3bd50538
SamSteele01 <ssteele017@gmail.com>
2019-04-25 16:47:57 -0400

    :sparkles: initial server files :construction: untested

---
+++ server/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+let mongoDB = process.env.MONGODB_URI || 'mongodb://mongo:27017/libraryOfBabel';
+
+mongoose.connect(
+  mongoDB,
+  { useNewUrlParser: true },
+);
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+
+// test MongoDB connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected to mongoDB.');
+});
+
+module.exports = db;
