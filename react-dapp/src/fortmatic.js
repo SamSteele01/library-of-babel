c8a3c656310166d533b415f815241ab4465122f5
SamSteele01 <ssteele017@gmail.com>
2019-04-25 16:47:57 -0400

    get web3 and return a string of the source

--- react-dapp/src/fortmatic.js
+++ react-dapp/src/fortmatic.js
@@ -5,5 +5,35 @@ import Web3 from 'web3';
 const fortmaticTestApiKey = "pk_test_FA4473198B4649E4"
 // const fortmaticMainApiKey = "pk_live_0AC92946C8888616"
 
-const fm = new Fortmatic(fortmaticTestApiKey);
-window.web3 = new Web3(fm.getProvider());
+async function getWeb3() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            // Request account access if needed
+            await window.ethereum.enable();
+            // Acccounts now exposed
+            console.log("MetaMask");
+            return "metamask";
+        } catch (error) {
+            // User denied account access...
+            console.log('USER DENIED ACCOUNT ACCESS');
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        // Acccounts always exposed
+        console.log("browser");
+        return "browser";
+    }
+    // Non-dapp browsers...
+    else {
+      const fm = new Fortmatic(fortmaticTestApiKey);
+      window.web3 = new Web3(fm.getProvider());
+      // window.web3.currentProvider.enable();
+      console.log("fortmatic");
+      return "fortmatic";
+    }
+};
+export default getWeb3;
