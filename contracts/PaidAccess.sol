bdf3fd3bc1cf9918a7964838d8179f1a6244482e
SamSteele01 <ssteele017@gmail.com>
2019-04-25 16:47:57 -0400

    create contract

---
+++ contracts/PaidAccess.sol
@@ -0,0 +1,9 @@
+
+
+// load new resource (price, locationURL, forwardingAddress) generates a resourceId
+
+// check resource - returns properties [price, locationURL, forwardingAddress?]
+
+// buy access (resourceId) payable - maps msg.sender to resourceId
+
+// check access (resourceId) checks that msg.sender is mapped to that resourceId
