5f9ae709627719926733999b89ad23f56d8a39a5
Aaron Anderson <aaronlynanderson@gmail.com>
2019-04-27 18:12:33 -0500

    :star: Sell component is from material-ui expansion pannel

--- react-dapp/src/components/Sell/Sell.js
+++ react-dapp/src/components/Sell/Sell.js
@@ -1,73 +1,100 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import classNames from "classnames";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
 import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 
-import BookIcon from "../../styles/book-icon.png";
-
-const styles = {
-  card: {
-    width: "94%",
-    margin: "3%"
+const styles = theme => ({
+  root: {
+    width: "100%"
   },
-  media: {
-    height: 140,
-    backgroundSize: "contain"
+  heading: {
+    fontSize: theme.typography.pxToRem(15)
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  },
+  icon: {
+    verticalAlign: "bottom",
+    height: 20,
+    width: 20
+  },
+  details: {
+    alignItems: "center"
+  },
+  column: {
+    flexBasis: "33.33%"
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline"
+    }
   }
-};
+});
 
-function Sell(props) {
-  const { classes, index, title, image, desc, price, purchase, view } = props;
-
+function DetailedExpansionPanel(props) {
+  const { classes } = props;
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <Typography gutterBottom variant="h5" component="h2">
-          SELL CARD
-        </Typography>
-        <CardMedia
-          className={classes.media}
-          image={image ? image : BookIcon}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {title}
-          </Typography>
-          <Typography component="p">{desc}</Typography>
-          <Typography component="p">$ {price} Eth</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        {purchase && (
-          <Button
-            variant="contained"
-            size="small"
-            color="primary"
-            onClick={event => purchase(true)}
-          >
-            Buy
+    <div className={classes.root}>
+      <ExpansionPanel defaultExpanded>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <div className={classes.column}>
+            <Typography className={classes.heading}>Location</Typography>
+          </div>
+          <div className={classes.column}>
+            <Typography className={classes.secondaryHeading}>
+              Select trip destination
+            </Typography>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.details}>
+          <div className={classes.column} />
+          <div className={classes.column}>
+            <Chip
+              label="Barbados"
+              className={classes.chip}
+              onDelete={() => {}}
+            />
+          </div>
+          <div className={classNames(classes.column, classes.helper)}>
+            <Typography variant="caption">
+              Select your destination of choice
+              <br />
+              <a href="#sub-labels-and-columns" className={classes.link}>
+                Learn more
+              </a>
+            </Typography>
+          </div>
+        </ExpansionPanelDetails>
+        <Divider />
+        <ExpansionPanelActions>
+          <Button size="small">Cancel</Button>
+          <Button size="small" color="primary">
+            Save
           </Button>
-        )}
-        {view && (
-          <Button size="small" color="primary" onClick={event => view(index)}>
-            Learn More
-          </Button>
-        )}
-      </CardActions>
-    </Card>
+        </ExpansionPanelActions>
+      </ExpansionPanel>
+    </div>
   );
 }
 
-Sell.propTypes = {
-  classes: PropTypes.object.isRequired,
-  title: PropTypes.string.isRequired
+DetailedExpansionPanel.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Sell);
+export default withStyles(styles)(DetailedExpansionPanel);
