b777fd8b91cf4a58d1381ae4131db21c89fb687e
Aaron Anderson <aaronlynanderson@gmail.com>
2019-04-27 13:57:31 -0500

    :mute: remove unused variables to silence warnings

--- react-dapp/src/components/Header/index.js
+++ react-dapp/src/components/Header/index.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./index.css";
 
 function Header() {
-  const [book, setBook] = useState(null);
   return (
     <div>
       <header className="App-header">
