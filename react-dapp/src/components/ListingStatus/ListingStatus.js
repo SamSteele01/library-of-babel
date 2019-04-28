d9dfd99bbbe3767a39b2cc0e83efbece0cd7bbcd
Aaron Anderson <aaronlynanderson@gmail.com>
2019-04-28 17:34:29 -0500

    :mute: remove unecessary divs

--- react-dapp/src/components/ListingStatus/ListingStatus.js
+++ react-dapp/src/components/ListingStatus/ListingStatus.js
@@ -16,17 +16,15 @@ function PaperSheet(props) {
   const { classes } = props;
 
   return (
-    <div>
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3">
-          This is a sheet of paper.
-        </Typography>
-        <Typography component="p">
-          Paper can be used to build surface or other elements for your
-          application.
-        </Typography>
-      </Paper>
-    </div>
+    <Paper className={classes.root} elevation={1}>
+      <Typography variant="h5" component="h3">
+        This is a sheet of paper.
+      </Typography>
+      <Typography component="p">
+        Paper can be used to build surface or other elements for your
+        application.
+      </Typography>
+    </Paper>
   );
 }
 
