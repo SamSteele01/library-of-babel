671c1bc897255d65550fa3616000432a9aab8c9b
SamSteele01 <ssteele017@gmail.com>
2019-04-30 02:53:19 -0400

    :index_card: add contract

--- contracts/PaidAccess.sol
+++ contracts/PaidAccess.sol
@@ -1,10 +1,122 @@
+pragma solidity ^0.5.7;
+
+/*
+ *  Author... Sam Steele
+ *  Email.... ssteele017@gmail.com
+ *  Date..... 4.28.19
+ */
+
+  import "./Ownable.sol"; // openZeppelin
+
+ contract PaidAccess is Ownable {
+
+     /**************************************************
+      *  Events
+      */
+     event ContentAdded(string labelHash, string ipfsPath, uint256 price);
+
+     event PriceChanged(string labelHash, uint256 price);
+
+     event ContentPurchased(string labelHash, address sender);
+
+     /**************************************************
+      *  Storage
+      */
+
+    // NOTE Label == labelHash
+
+    struct Content {
+      // string labelHash;
+      string ipfsPath;
+      address owner;
+      uint256 price;
+      mapping (address => bool) hasPaid;
+    }
+
+    mapping (string => Content) contents; // labelHash
+
+    // Inventory
+    uint256 public numberOfContents;
+    mapping (uint256 => string) contentRegistry; // labelHash
+
+    uint8 public feePercent; // 1 - 100
+
+    constructor () public {
+        feePercent = 3;
+    }
 
 
-// load new resource (price, locationURL, forwardingAddress) generates a resourceId
-function addContent(ethAddress, ipfsPath, label, price) public 
+    /**************************************************
+      *  Fallback
+      */
 
-// check resource - returns properties [price, locationURL, forwardingAddress?]
+     function() external payable {
+          revert();
+     }
 
-// buy access (resourceId) payable - maps msg.sender to resourceId
+    /**************************************************
+      *  External
+      */
 
-// check access (resourceId) checks that msg.sender is mapped to that resourceId
+      function addContent(string calldata labelHash, string calldata ipfsPath, uint256 price) external {
+          contents[labelHash] = Content(ipfsPath, msg.sender, price);
+          numberOfContents += 1;
+          contentRegistry[numberOfContents] = labelHash;
+          emit ContentAdded(labelHash, ipfsPath, price);
+      }
+
+      function changePrice(string calldata labelHash, uint256 price) external {
+          require(contents[labelHash].owner == msg.sender);
+          contents[labelHash].price = price;
+          emit PriceChanged(labelHash, price);
+      }
+
+      function purchaseContent(string calldata labelHash) external payable {
+          require(msg.value >= contents[labelHash].price);
+          require(!contents[labelHash].hasPaid[msg.sender], "You have already paid for this.");
+          _forwardPayment(contents[labelHash].owner, msg.value);
+          contents[labelHash].hasPaid[msg.sender] = true;
+          emit ContentPurchased(labelHash, msg.sender);
+      }
+
+      function changeFee(uint8 newFeePercent) external onlyOwner {
+          require(newFeePercent <= 100, "New fee is too high.");
+          require(newFeePercent >= 0, "Fee can not be negative.");
+          feePercent = newFeePercent;
+      }
+
+     /**************************************************
+      *  Public
+      */
+
+      // check resource - returns properties [price, locationURL, forwardingAddress?]
+      function getContentByLabel(string memory labelHash) public view
+      returns (string memory _ipfsPath, uint256 _price) {
+          _ipfsPath = contents[labelHash].ipfsPath;
+          _price = contents[labelHash].price;
+      }
+
+      function getLabelByIndex(uint256 index) public view returns(string memory) {
+        return contentRegistry[index];
+      }
+
+      function checkHasPaid(string memory labelHash, address buyer) public view returns(bool) {
+          return contents[labelHash].hasPaid[buyer];
+      }
+
+      function withdraw(uint256 amount) public onlyOwner {
+          require(amount <= address(this).balance);
+          msg.sender.transfer(amount);
+      }
+
+     /**************************************************
+      *  Internal
+      */
+
+      function _forwardPayment(address receiver, uint256 payment) private {
+          // take fee
+          uint256 forwardAmount = payment - payment * feePercent / 100;
+          address(uint160(receiver)).transfer(forwardAmount);
+      }
+
+ }
---
+++ contracts/PaidAccessAbi.js
@@ -0,0 +1,313 @@
+const PaidAccessAbi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"name": "ipfsPath",
+				"type": "string"
+			},
+			{
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "addContent",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newFeePercent",
+				"type": "uint8"
+			}
+		],
+		"name": "changeFee",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "changePrice",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "labelHash",
+				"type": "string"
+			}
+		],
+		"name": "purchaseContent",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "ipfsPath",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "ContentAdded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "PriceChanged",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "ContentPurchased",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "labelHash",
+				"type": "string"
+			},
+			{
+				"name": "buyer",
+				"type": "address"
+			}
+		],
+		"name": "checkHasPaid",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "feePercent",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "labelHash",
+				"type": "string"
+			}
+		],
+		"name": "getContentByLabel",
+		"outputs": [
+			{
+				"name": "_ipfsPath",
+				"type": "string"
+			},
+			{
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getLabelByIndex",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "isOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "numberOfContents",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+module.exports PaidAccessAbi;
